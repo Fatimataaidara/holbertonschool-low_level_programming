516d191df9b8a7c3d32934ac7547bf4561b5751f
MoustaphaElPsyCongroo <mtapha.tall@gmail.com>
2024-03-01 13:25:39 +0100

    projet2

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -1,23 +1,27 @@
 #include "main.h"
+
 /**
- * _strncpy - concatenate strings, defining the size of second string.
- * @dest: string with concatenation
- * @src: string to be concatenated
- * @n: size of second string
- * Return: Always 0.
+ * _strncpy - copy a string, exactly like strncpy
+ * @dest: the pointer contetn destination string
+ * @src: the pointer content the string source to copy
+ * @n: the number of the string to add to dest
+ *
+ * Return: char
  */
 char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
 
 	for (i = 0; i < n && src[i] != '\0'; i++)
-	
-		*(dest + i) = *(src + i);
+	{
+		dest[i] = src[i];
+	}
 
-	for ( ; i < n; i++)
-
-		*(dest + i) = '\0';
+	while  (i < n)
+	{
+		dest[i] = '\0';
+		i++;
+	}
 
 	return (dest);
-
 }
